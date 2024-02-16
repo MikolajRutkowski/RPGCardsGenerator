2322d0129648bc5bbb7d7bab487d6b0a7a8939db
mikolaj <mikolaj99x@gmailcom>
2024-02-16 21:10:00 +0100

    Add project files.

---
+++ Variables/Character.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+
+namespace RPGCardsGenerator.Variables
+{
+    internal abstract class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Statistics> Characteristics = new List<Statistics>();
+        public Character() {
+            Characteristics.Add(new Statistics("Siła", TypeOfCariables.characteristic));
+        }
+        
+    }
+}
---
+++ Variables/Statistics.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGCardsGenerator.Variables
+{
+    public enum TypeOfCariables
+    {
+        characteristic,
+        skill
+    }
+
+    internal class Statistics
+    {
+        public int Id {  get; set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; } = string.Empty;
+        public int Value { get; private set; }
+        TypeOfCariables Type { get;  set; }
+        public Statistics(string name, TypeOfCariables type, int value = 1) {
+
+            this.Name = name;
+            this.Value = value;
+            this.Type = type;
+        }
+        
+
+    }
+}
